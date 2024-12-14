bc41b532c808f847d83ec0f8569c38b97f0a0456
jianyuoo <108246265+jianyuoo@users.noreply.github.com>
2024-12-14 19:54:06 +0800

    病例实体类

---
+++ src/main/java/air/admin/spring_boot/base/cases/entity/Cases.java
@@ -0,0 +1,53 @@
+package air.admin.spring_boot.base.cases.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+/**
+ * 病例表实体类
+ * */
+@Data
+@TableName(value = "mzgl_cases")
+public class Cases {
+    /**
+     *  病例id
+     * */
+    private Long caseid;
+    /**
+     *  病例名称
+     * */
+    private String casename;
+    /**
+     *  患者id
+     * */
+    private Long patientid;
+    /**
+     *  医生id
+     * */
+    private Long doctorid;
+    /**
+     *  检查单id
+     * */
+    private Long examineid;
+    /**
+     *  病例建立时间
+     * */
+    private LocalDateTime casecreatetime;
+    /**
+     *  病例修改时间
+     * */
+    private LocalDateTime caseupdatetime;
+    /**
+     *  病例数据
+     * */
+    private String casedata;
+    /**
+     *  药品id
+     * */
+    private Long drugid;
+    /**
+     *  药品数据
+     * */
+    private String drugdata;
+}
