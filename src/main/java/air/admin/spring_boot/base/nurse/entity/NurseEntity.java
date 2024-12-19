09c6887a5dd213ee709915e254bab19e1d182a80
liGaos <976615018@qq.com>
2024-12-19 17:37:35 +0800

    增查

--- src/main/java/air/admin/spring_boot/base/nurse/entity/NurseEntity.java
+++ src/main/java/air/admin/spring_boot/base/nurse/entity/NurseEntity.java
@@ -1,15 +1,17 @@
 package air.admin.spring_boot.base.nurse.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName(value = "mzgl_nurse")
 public class NurseEntity {
 
-    @TableField(value = "nurse_id")
+    @TableField(value = "id")
     private int nurseId;//护士id
 
-    @TableField(value = "nurse_name")
+    @TableField(value = "name")
     private String nurseName;//护士姓名
 
     @TableField(value = "jobtitle")
